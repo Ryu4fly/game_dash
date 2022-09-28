aebaa27ee72e8d6652f7d56fc93823946f981f9c
ryu4fly <rsuguro@gmail.com>
2022-09-28 23:20:55 +0900

    [DB] Added connection to MongoDB using Mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
+import connectDB from './db/connectdb';
+
 dotenv.config();
 
 const port = process.env.PORT;
+const mongoDbUrl = process.env.MONGODB_URL as string;
 const app = express();
 
-app.listen(port, () => {
-  console.log(`Listening to port: ${port} ...`)
-});
+(async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Listening to port: ${port} ...`);
+    });
+    await connectDB(mongoDbUrl);
+  } catch (error) {
+    console.log(error);
+  }
+})();

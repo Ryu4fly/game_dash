3aac9d17c8d395e708b48c9f8a6b7e34b4a58b69
ryu4fly <rsuguro@gmail.com>
2022-09-29 23:52:00 +0900

    [Models] Added more ref to UserOwnedGame model

--- src/models/user_owned_games.ts
+++ src/models/user_owned_games.ts
@@ -1,20 +1,29 @@
 import mongoose from 'mongoose';
 
+import GameDetail from './game_details';
 import User from './users';
 
 const schema = new mongoose.Schema({
   app_id: {
-    type: Number
+    type: Number,
+    required: true
   },
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: User
+    ref: User,
+    required: true
   },
   playtime_forever: {
-    type: Number
+    type: Number,
+    required: true
   },
   last_played: {
-    type: Date
+    type: Date,
+    required: true
+  },
+  game_detail: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: GameDetail
   }
 });
 

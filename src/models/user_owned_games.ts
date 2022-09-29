316434d07c28401333a1307def8624e994a027f8
ryu4fly <rsuguro@gmail.com>
2022-09-29 23:42:46 +0900

    [Models] Added User relationship to UserOwnedGames and UserProfiles

--- src/models/user_owned_games.ts
+++ src/models/user_owned_games.ts
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
+import User from './users';
+
 const schema = new mongoose.Schema({
   app_id: {
     type: Number
   },
   user_id: {
-    type: Number
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User
   },
   playtime_forever: {
     type: Number
--- src/models/user_profiles.ts
+++ src/models/user_profiles.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
+import User from './users';
+
 const schema = new mongoose.Schema({
   user_id: {
-    type: Number
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User
   },
   first_name: {
     type: String,
